9f4b4ba32992f13aa152f6537f49cb46024e7787
LimNoah <limnoah0301@gmail.com>
2022-05-16 17:32:37 +0900

    docs: <8.2.3> Modify Info.js code
    - Modify useEffect code.

--- src/Info.js
+++ src/Info.js
@@ -5,13 +5,30 @@ const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
 
+    // useEffect(() => {
+    //     console.log('렌더링이 완료되었습니다.');
+    //     console.log({
+    //         name,
+    //         nickname
+    //     });
+    // });
+
+    // useEffect(() => {
+    //     console.log('마운트 될 때만 실행된다.')
+    // }, []);
+
+    // useEffect(() => {
+    //     console.log('name', name)
+    // }, [name]);
+
     useEffect(() => {
-        console.log('렌더링이 완료되었습니다.');
-        console.log({
-            name,
-            nickname
-        });
-    });
+        console.log('effect');
+        console.log(name);
+        return () => {
+            console.log('cleanup');
+            console.log(name);
+        };
+    }, [name]);
 
     const onChangeName = e => {
         setName(e.target.value);
